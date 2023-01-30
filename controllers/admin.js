74f4a65e74da5a3a6c9150f34e4d75ca58393efc
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-01-30 12:41:25 +0530

    219. Using relations in Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user,
   });
   // leftside defines keys in the schema
   // rightside defines data we recieve
